b52989374955923e10b423c894ab9cd4b8bfb892
CosmaAndrei <57963409+CosmaAndrei@users.noreply.github.com>
2020-02-16 20:49:37 +0200

    Created image processing service class.

---
+++ src/main/java/com/cv/imagetron/service/def/ImageProcessingService.java
@@ -0,0 +1,4 @@
+package com.cv.imagetron.service.def;
+
+public interface ImageProcessingService {
+}
---
+++ src/main/java/com/cv/imagetron/service/impl/ImageProcessingServiceImpl.java
@@ -0,0 +1,6 @@
+package com.cv.imagetron.service.impl;
+
+import com.cv.imagetron.service.def.ImageProcessingService;
+
+public class ImageProcessingServiceImpl implements ImageProcessingService {
+}
