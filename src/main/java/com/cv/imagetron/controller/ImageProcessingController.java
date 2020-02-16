f92b690bd5d3afccca03c70b9fedee474a30c09b
CosmaAndrei <57963409+CosmaAndrei@users.noreply.github.com>
2020-02-16 22:51:47 +0200

    Added implementation for blur controller.

--- src/main/java/com/cv/imagetron/controller/ImageProcessingController.java
+++ src/main/java/com/cv/imagetron/controller/ImageProcessingController.java
@@ -1,10 +1,30 @@
 package com.cv.imagetron.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cv.imagetron.service.def.ImageProcessingService;
+import com.cv.imagetron.util.Util;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RequestMapping("/image-processing-management")
 @RestController
 public class ImageProcessingController {
 
+    @Autowired
+    ImageProcessingService imageProcessingService;
+    @Autowired
+    Util util;
+
+    @PostMapping(value = "/blur",
+            produces = MediaType.IMAGE_PNG_VALUE)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public File blurImage(@RequestBody MultipartFile file) throws IOException {
+        return imageProcessingService.blurImage(util.multipartToFile(file, "convertedFileName"));
+    }
 }
