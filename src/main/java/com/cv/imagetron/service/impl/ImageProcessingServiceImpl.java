1a476780c8b2c25e27684569776d005f44c6c72f
CosmaAndrei <57963409+CosmaAndrei@users.noreply.github.com>
2020-02-16 22:09:23 +0200

    Added removeNoiseFrom implementation.

--- src/main/java/com/cv/imagetron/service/impl/ImageProcessingServiceImpl.java
+++ src/main/java/com/cv/imagetron/service/impl/ImageProcessingServiceImpl.java
@@ -1,9 +1,11 @@
 package com.cv.imagetron.service.impl;
 
+import boofcv.abst.denoise.FactoryImageDenoise;
+import boofcv.abst.denoise.WaveletDenoiseFilter;
 import boofcv.alg.filter.blur.GBlurImageOps;
-import boofcv.io.UtilIO;
 import boofcv.io.image.ConvertBufferedImage;
 import boofcv.io.image.UtilImageIO;
+import boofcv.struct.image.GrayF32;
 import boofcv.struct.image.GrayU8;
 import boofcv.struct.image.ImageType;
 import boofcv.struct.image.Planar;
@@ -31,9 +33,34 @@ public class ImageProcessingServiceImpl implements ImageProcessingService {
         return outputFile;
     }
 
+    /**
+     * Example of how to "remove" noise from images using wavelet based algorithms.  A simplified interface is used
+     * which hides most of the complexity.  Wavelet image processing is still under development and only floating point
+     * images are currently supported.  Which is why the image  type is hard coded.
+     */
     @Override
-    public File removeNoiseFrom(File image) {
-        return null;
+    public File removeNoiseFrom(File image) throws IOException {
+        // load the input image, declare data structures
+        GrayF32 input = UtilImageIO.loadImage(image.getAbsolutePath(), GrayF32.class);
+
+        GrayF32 denoised = input.createSameShape();
+
+        // How many levels in wavelet transform
+        int numLevels = 4;
+        // Create the noise removal algorithm
+        WaveletDenoiseFilter<GrayF32> denoiser =
+                FactoryImageDenoise.waveletBayes(GrayF32.class, numLevels, 0, 255);
+
+        // remove noise from the image
+        denoiser.process(input, denoised);
+
+        // display the results
+        BufferedImage denoisedImage = ConvertBufferedImage.convertTo(denoised, null);
+
+        File outputFile = new File("denoisedImage.png");
+        ImageIO.write(denoisedImage, "png", outputFile);
+
+        return outputFile;
     }
 
     @Override
