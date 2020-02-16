663740e1b792a657c707cc94666188d7cc850e39
CosmaAndrei <57963409+CosmaAndrei@users.noreply.github.com>
2020-02-16 20:44:54 +0200

    Initial commit.

---
+++ src/main/java/com/cv/imagetron/ImagetronApplication.java
@@ -0,0 +1,13 @@
+package com.cv.imagetron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagetronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagetronApplication.class, args);
+	}
+
+}
