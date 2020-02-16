ed6c4972168ea1fdaa54515a5814e0b5e04ff49c
CosmaAndrei <57963409+CosmaAndrei@users.noreply.github.com>
2020-02-16 21:10:16 +0200

    Added exception signature.

--- src/main/java/com/cv/imagetron/service/def/ImageProcessingService.java
+++ src/main/java/com/cv/imagetron/service/def/ImageProcessingService.java
@@ -1,4 +1,12 @@
 package com.cv.imagetron.service.def;
 
+import java.io.File;
+import java.io.IOException;
+
 public interface ImageProcessingService {
+    File blurImage(File image) throws IOException;
+
+    File removeNoiseFrom(File image) throws IOException;
+
+    File convertToFishEye(File image) throws IOException;
 }
