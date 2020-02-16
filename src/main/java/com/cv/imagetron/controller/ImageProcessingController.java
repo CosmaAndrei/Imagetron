a8ec3cb2ace46a777fc7f610ba859e85426117d3
CosmaAndrei <57963409+CosmaAndrei@users.noreply.github.com>
2020-02-16 20:49:22 +0200

    Created Controller class.

---
+++ src/main/java/com/cv/imagetron/controller/ImageProcessingController.java
@@ -0,0 +1,10 @@
+package com.cv.imagetron.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/image-processing-management")
+@RestController
+public class ImageProcessingController {
+
+}
